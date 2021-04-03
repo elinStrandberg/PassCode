fbe5b6b6732554a238323f196190c02a7beee262
Elin Strandberg <elin-strandberg@hotmail.com>
2021-04-03 12:41:41 +0200

    Initial version commit

---
+++ PassCode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PassCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = "";
+
+            while (code != "secret")
+            {
+                Console.WriteLine("What is the pass code?");
+                code = Console.ReadLine();
+                
+                if(code != "secret")
+                {
+                    Console.WriteLine("Not Authenticated");
+                }
+            }
+            Console.WriteLine("Authenticated");
+        }
+    }
+}
